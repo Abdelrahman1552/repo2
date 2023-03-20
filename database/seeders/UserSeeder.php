ec14861f7cbd6266931e081ed8b4f66c9996502e
3abdelrahman <102231093+Abdelrahman1552@users.noreply.github.com>
2023-03-21 00:44:14 +0200

    Update UserSeeder.php

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -19,6 +19,8 @@ class UserSeeder extends Seeder
             'email' =>'Admin@gmail.com',
             'password' => Hash::make('12345678'),
             'remember_token' => Str::random(20),
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s"),
             'role_as' => "1",
         ]);
     }
