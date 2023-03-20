28e1919d23e42060be0d2eda50fe2e7614674bed
3abdelrahman <102231093+Abdelrahman1552@users.noreply.github.com>
2023-03-20 19:59:28 +0200

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,12 +7,12 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
+            <div class="bg-white light:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-dark-900 dark:text-dark-100">
                     @if (session("status"))
                         <div class="alert alert-success">{{session('status')}}</div>
                     @endif
-                    {{ __("You're logged in! this page for User") }}
+                    {{ __("You're logged in! ") }}
                 </div>
             </div>
         </div>
